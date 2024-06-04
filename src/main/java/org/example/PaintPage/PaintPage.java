d58a12263922d820675558b69183acda1a75bfd4
Timyu0527 <st970387@gmail.com>
2024-06-04 21:37:06 +0800

    fix a lot of bugs

--- src/main/java/org/example/PaintPage/PaintPage.java
+++ src/main/java/org/example/PaintPage/PaintPage.java
@@ -18,7 +18,7 @@ public class PaintPage {
         frame.add(drawPanel, BorderLayout.CENTER);
         frame.add(buttomBar, BorderLayout.SOUTH);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(800, 600); 
         frame.setVisible(true);
     }
